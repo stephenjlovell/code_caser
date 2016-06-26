763395203997ea7ff043098df30ed57603ebcbb2
Stephen Lovell <sjlovell34@gmail.com>
2016-06-26 15:54:48 -0400

    clean up specs and debug

--- lib/code_caser/caser.rb
+++ lib/code_caser/caser.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module CodeCaser
 
   class Caser
@@ -8,13 +10,14 @@ module CodeCaser
     end
 
     def start
-      files = Dir.glob(@target)
-      unless files.empty?
+      if File.directory?(@target)
         puts "converting files from #{@converter.description}:"
-        files.each do |f|
+        Dir[@target + "/*"].each do |f|
           convert_file(f)
           puts "-> #{f}"
         end
+      elsif File.file?(@target)
+        convert_file(@target)
       else
         puts "file or folder location not found: #{@target}"
       end
@@ -24,6 +27,7 @@ module CodeCaser
       # if the option is set, preserve the original file.
       original = File.join(File.dirname(file_path), File.basename(file_path, ".*") +
         "_old" + File.extname(file_path))
+      puts file_path, original
       FileUtils.cp(file_path, original)
       FileUtils.rm(file_path)
 
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -6,28 +6,31 @@ describe CodeCaser do
 
     describe CodeCaser::CamelConverter do
       let(:conv) { CodeCaser::CamelConverter.new }
-
       it "should safely convert camelCase identifiers to snake_case" do
-        expect(conv.convert_line("camelCase += 1")).to       eq("camel_case += 1")
-        expect(conv.convert_line("HTTPIsAnAcronym")).to eq("http_is_an_acronym")
-        expect(conv.convert_line("getHTTPResponse")).to eq("get_http_response")
-        expect(conv.convert_line("_camelCase_")).to     eq("_camel_case_")
-        expect(conv.convert_line("__camelCase__")).to   eq("__camel_case__")
+        {
+          "camelCase += 1"  => "camel_case += 1",
+          "HTTPIsAnAcronym" => "http_is_an_acronym",
+          "getHTTPResponse" => "get_http_response",
+          "_camelCase_"     => "_camel_case_",
+          "__camelCase__"   => "__camel_case__"
+        }.each do |camel, snake|
+          expect(conv.convert_line(camel)).to eq(snake)
+        end
       end
     end
 
     describe CodeCaser::SnakeConverter do
       let(:conv) { CodeCaser::SnakeConverter.new }
-
       it "should safely convert snake_case identifiers to camelCase" do
-        expect(conv.convert_line("snake_case += 1")).to           eq("snakeCase += 1")
-        expect(conv.convert_line("alreadyCamelCase")).to          eq("alreadyCamelCase")
-        # expect(conv.convert_line("")).to    eq("")
-        expect(conv.convert_line("doubles__become_single")).to    eq("doubles_BecomeSingle")
-        expect(conv.convert_line("_outside_underscores_preserved_")).to(
-          eq("_outsideUnderscoresPreserved_"))
+        {
+          "snake_case += 1"                 => "snakeCase += 1",
+          "alreadyCamelCase"                => "alreadyCamelCase",
+          "doubles__left_intact"            => "doubles__leftIntact",
+          "_outside_underscores_preserved_" => "_outsideUnderscoresPreserved_"
+        }.each do |snake, camel|
+          expect(conv.convert_line(snake)).to eq(camel)
+        end
       end
-
     end
   end
 end
