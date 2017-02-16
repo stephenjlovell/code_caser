6d99859a34e36f557715815ec95e2f5d446bea7a
Stephen Lovell <sjlovell34@gmail.com>
2017-02-16 18:23:13 -0500

    move file conversion methods to separate namespace

--- lib/code_caser.rb
+++ lib/code_caser.rb
@@ -2,12 +2,14 @@
 Dir[File.dirname(__FILE__) + '/code_caser/*.rb'].each { |file| require file }
 
 module CodeCaser
-  def self.to_camel(opts)
-    Caser.new(opts.merge(converter: SnakeConverter.new(opts))).start
-  end
+  module FileConversion
+    def self.to_camel(opts)
+      Caser.new(opts.merge(converter: SnakeConverter.new(opts))).start
+    end
 
-  def self.to_snake(opts)
-    Caser.new(opts.merge(converter: CamelConverter.new(opts))).start
+    def self.to_snake(opts)
+      Caser.new(opts.merge(converter: CamelConverter.new(opts))).start
+    end
   end
 
   module Analyze
